ca949926298529b6e6257a47b59ef6852c274ef5
Javier Collipal <jcollipal1212@gmail.com>
2025-06-01 22:33:17 -0400

    testing cors solution

--- src/main.ts
+++ src/main.ts
@@ -14,21 +14,18 @@ async function bootstrap() {
 
   // Enable CORS with credentials
   app.enableCors({
-    origin: process.env.ALLOWED_ORIGINS?.split(',') || true,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: [
-      'Content-Type',
-      'Accept',
-      'Authorization',
-      'Access-Control-Allow-Origin',
-      'Access-Control-Request-Method',
-      'Access-Control-Request-Headers',
-      'Origin',
+    origin: [
+      'http://localhost:8081',
+      'http://localhost:3000',
+      'http://localhost:4000',
+      'http://localhost:19006',
+      'http://localhost:8001',
+      'https://sunlit-garden-api-ec124189a937.herokuapp.com',
     ],
-    exposedHeaders: ['Access-Control-Allow-Origin'],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
+    credentials: false,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Accept,Authorization',
+    maxAge: 86400, // 24 hours in seconds
   });
 
   // Add logging for debugging
